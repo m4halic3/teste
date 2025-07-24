7411eab95cb160dfd08bc320698ad26357a208fd
m4halic3 <mariana.alice667@gmail.com>
2025-07-24 15:54:05 -0300

    feat: starts project with vite

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>Hello world</h1>
+        <button>Fazer requisção</button>
+      </div>
+    </>
+  )
+}
+
+export default App
